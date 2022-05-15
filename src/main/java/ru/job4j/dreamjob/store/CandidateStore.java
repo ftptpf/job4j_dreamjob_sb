1fa6baed9d33194178ba6582a521f98d5f5bc61b
ftptpf <Nb49Rtdd>
2022-05-15 14:00:05 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore INST = new CandidateStore();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Сергей", "JAVA, SQL, Spring", LocalDateTime.now()));
+        candidates.put(2, new Candidate(2, "Виктор", "PM", LocalDateTime.now()));
+        candidates.put(3, new Candidate(3, "Ольга", "менеджер", LocalDateTime.now()));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}

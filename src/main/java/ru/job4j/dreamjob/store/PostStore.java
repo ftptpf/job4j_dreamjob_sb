825c819a0bf2d4fe4f9c91ebbc2222d383fec33b
ftptpf <inboxru@yandex.ru>
2022-05-16 09:44:00 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,10 +6,13 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
     private static final PostStore INST = new PostStore();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private static final AtomicInteger POST_ID = new AtomicInteger(4);
+
 
     private PostStore() {
         posts.put(1, new Post(1, "Junior Java Job", "Вакансия в компании Union", LocalDateTime.now()));
@@ -23,5 +26,12 @@ public class PostStore {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public void add(Post post) {
+        if (post.getId() == 0) {
+            post.setId(POST_ID.incrementAndGet());
+        }
+        posts.put(post.getId(), post);
     }
 }

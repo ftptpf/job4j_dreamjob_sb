bb08626befb340c2411473d2fbea16b0cb3dda13
ftptpf <inboxru@yandex.ru>
2022-05-20 08:27:44 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -8,6 +8,7 @@ public class Post {
     private String name;
     private String description;
     private LocalDateTime created;
+    private boolean visible;
 
     public Post() {
     }
@@ -60,6 +61,14 @@ public class Post {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override

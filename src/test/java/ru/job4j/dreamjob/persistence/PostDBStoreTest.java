f868c55d520a457ac557bbd15e33055345bdf8b1
ftptpf <Nb49Rtdd>
2022-05-24 21:37:20 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862]

---
+++ src/test/java/ru/job4j/dreamjob/persistence/PostDBStoreTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.dreamjob.persistence;
+
+import org.junit.Test;
+import ru.job4j.dreamjob.Main;
+import ru.job4j.dreamjob.model.Post;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PostDBStoreTest {
+
+    @Test
+    public void whenCreatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job");
+        store.add(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+}

3bf4e9d48d130567bf81d30b8d1819102e9801b3
ftptpf <inboxru@yandex.ru>
2022-05-27 09:52:34 +0300

    4. Многопоточность в базе данных [#504860]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -14,3 +14,11 @@ CREATE TABLE candidate (
     created TIMESTAMP,
     photo BYTEA
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT,
+    password TEXT
+);
+
+ALTER TABLE users ADD CONSTRAINT email_unique UNIQUE (email);

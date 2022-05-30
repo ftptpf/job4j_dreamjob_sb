06199c95634ff803c474e3f16e21dd53a2a1f36b
ftptpf <Nb49Rtdd>
2022-05-30 21:30:30 +0300

    4. Многопоточность в базе данных [#504860] - test (try-catch-finally)

--- src/test/java/ru/job4j/dreamjob/persistence/UserDbStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/persistence/UserDbStoreTest.java
@@ -5,7 +5,9 @@ import ru.job4j.dreamjob.Main;
 import ru.job4j.dreamjob.model.User;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+
+import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 
 public class UserDbStoreTest {
     private final User userOne = new User("mail@mail.ru", "password_1");
@@ -23,8 +25,14 @@ public class UserDbStoreTest {
     @Test
     public void whenCreateUsersWithSameEmails() {
         UserDbStore store = new UserDbStore(new Main().loadPool());
-        store.add(userOne);
-        store.add(userTwo);
-        store.deleteAll();
+        try {
+            store.add(userOne);
+            store.add(userTwo);
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString(
+                    "org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException: Нарушение уникального индекса или первичного ключа"));
+        } finally {
+            store.deleteAll();
+        }
     }
 }

9b6f43c17f9c75ab81868b1ee53541350b87c975
ftptpf <inboxru@yandex.ru>
2022-05-30 07:44:07 +0300

    4. Многопоточность в базе данных [#504860] - IF NOT EXISTS

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,4 @@
-CREATE TABLE post (
+CREATE TABLE IF NOT EXISTS post (
     id SERIAL PRIMARY KEY,
     name TEXT,
     description TEXT,
@@ -7,7 +7,7 @@ CREATE TABLE post (
     city_id INTEGER
 );
 
-CREATE TABLE candidate (
+CREATE TABLE IF NOT EXISTS candidate (
     id SERIAL PRIMARY KEY,
     name TEXT,
     description TEXT,
@@ -15,8 +15,8 @@ CREATE TABLE candidate (
     photo BYTEA
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE ,
+    email TEXT UNIQUE,
     password TEXT
 );

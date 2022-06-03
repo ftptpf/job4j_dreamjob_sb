933c4e39aa6efd0f7c129d6f164213ebc7aabefd
ftptpf <inboxru@yandex.ru>
2022-06-03 09:45:56 +0300

    2. Тестируем PostController [#504867] - 3 tests

--- src/test/java/ru/job4j/dreamjob/control/PostControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/control/PostControllerTest.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.control;
 
 import org.junit.Test;
 import org.springframework.ui.Model;
+import ru.job4j.dreamjob.model.City;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.PostService;
@@ -23,13 +24,57 @@ public class PostControllerTest {
                 new Post(2, "New post")
         );
         Model model = mock(Model.class);
-        PostService service = mock(PostService.class);
-        when(service.findAll()).thenReturn(posts);
-        CityService cities = mock(CityService.class);
-        PostController postController = new PostController(service, cities);
         HttpSession session = mock(HttpSession.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(postService, cityService);
+
+        when(postService.findAll()).thenReturn(posts);
+
         String page = postController.posts(model, session);
         verify(model).addAttribute("posts", posts);
         assertThat(page, is("posts"));
+    }
+
+    @Test
+    public void whenAddPost() {
+        Post post = new Post(0, "Заполните поле");
+        Collection<City> cities = Arrays.asList(
+                new City(1, "Москва"),
+                new City(2, "Калининград")
+        );
+        Model model = mock(Model.class);
+        HttpSession session = mock(HttpSession.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(postService, cityService);
+
+        when(cityService.getAllCities()).thenReturn(cities);
+
+        String page = postController.addPost(model, session);
+        verify(model).addAttribute("post", post);
+        verify(model).addAttribute("cities", cities);
+        assertThat(page, is("addPost"));
+
+    }
+
+    @Test
+    public void whenCreatePost() {
+        City city = new City(0, "Москва");
+        Post input = new Post(1, "New post");
+        input.setCity(city);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(postService, cityService);
+        String page = postController.createPost(input);
+        verify(postService).add(input);
+        assertThat(page, is("redirect:/posts"));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        Post postOne = new Post();
+        Post postTwo = new Post();
+
     }
 }
